596e894d74bfcf971b4f718bc16afbe2033790b9
Fujisawa Shin <fujisawa-shin@kayac.com>
2013-07-26 16:35:40 +0900

    optsにrootも付けられるようにした。

--- tasks/koko.js
+++ tasks/koko.js
@@ -1,11 +1,18 @@
 var Koko = require('koko');
 
 module.exports = function (grunt) {
-    grunt.registerMultiTask('koko', 'start up web server (proxy server)', function () {
-        var target = this.target;
-        var done = this.async();
+    var taskName = 'koko',
+        taskDescription = 'start up web server (proxy server)';
 
-        var koko = new Koko(process.cwd(), grunt.config('koko')[target]);
+    grunt.registerMultiTask(taskName, taskDescription, function () {
+        var target = this.target,
+            config = grunt.config(taskName)[target],
+            done = this.async(),
+
+            root = config.root || process.cwd(),
+            opts = config;
+
+        var koko = new Koko(root, opts);
         koko.start();
     });
 };
