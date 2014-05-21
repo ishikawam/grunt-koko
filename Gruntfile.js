e418c74be55d5cf90493abdd4197a6804dec70fd
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-05-22 00:24:24 +0900

    grunt-releaseの導入してました

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,11 @@
 module.exports = function (grunt) {
-    grunt.loadTasks('tasks');
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        release: {
+            options: { }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-release');
+    grunt.registerTask('default', ['release:patch']);
 };
